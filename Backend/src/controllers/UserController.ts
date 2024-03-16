bd1c57f02c3f720c754110684d7f789e5477d54e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-16 21:22:01 +0400

    Create UserController.ts

---
+++ Backend/src/controllers/UserController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+
+class UserController {
+  // Method for creating a new user
+  async createUser(req: Request, res: Response) {
+    // Implementation to create a new user
+  }
+
+  // Method for getting user details
+  async getUserDetails(req: Request, res: Response) {
+    // Implementation to get user details
+  }
+
+  // Method for updating a user
+  async updateUser(req: Request, res: Response) {
+    // Implementation to update a user
+  }
+
+  // Method for logging in a user
+  async loginUser(req: Request, res: Response) {
+    // Implementation to log in a user
+  }
+
+  // Method for logging out a user
+  async logoutUser(req: Request, res: Response) {
+    // Implementation to log out a user
+  }
+
+  // Method for deleting a user
+  async deleteUser(req: Request, res: Response) {
+    // Implementation to delete a user
+  }
+}
+
+export default UserController;
