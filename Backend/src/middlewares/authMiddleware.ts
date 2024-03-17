90fcd8c3f669d8f1cf2e0440be859fd80d95fdf5
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-18 02:45:21 +0400

    Update authMiddleware.ts

--- Backend/src/middlewares/authMiddleware.ts
+++ Backend/src/middlewares/authMiddleware.ts
@@ -1,39 +1,36 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
-import { AuthenticationError, UserNotFoundError } from './errorMiddleware';
-import { User } from '../models/User';
+import { Request, Response, NextFunction } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import { AuthenticationError } from './errorMiddleware'
+import { User } from '../models/User'
 
 export const authenticateUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let token = req.cookies.jwt;
+      let token = req.cookies.jwt
 
       if (!token) {
-        throw new AuthenticationError('Token is required');
+        throw new AuthenticationError('Token is required')
       }
-      const jwtSecret = process.env.JWT_SECRET_KEY || '';
-      const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
+      const jwtSecret = process.env.JWT_SECRET_KEY || ''
+      const decoded = jwt.verify(token, jwtSecret) as JwtPayload
 
       if (!decoded || !decoded.userId) {
-        throw new AuthenticationError('User not found');
+        throw new AuthenticationError('User not found')
       }
 
-      const user = await User.findById(
-        decoded.userId,
-        '_id name email verified'
-      );
+      const user = await User.findById(decoded.userId, '_id username email verification')
       if (!user) {
-        throw new AuthenticationError('User not found');
+        throw new AuthenticationError('User not found')
       }
-      res.user = user;
-      next();
+      res.user = user
+      next()
     } catch (err) {
       if (err instanceof AuthenticationError) {
-        return next(err);
+        return next(err)
       } else {
-        throw new AuthenticationError('Invalid token');
+        throw new AuthenticationError('Invalid token')
       }
     }
-  }
-);
+  },
+)
