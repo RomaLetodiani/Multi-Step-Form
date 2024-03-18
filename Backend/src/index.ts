b243b54a4621476fe7d8e5aa1d15dbaefc982b98
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-19 01:25:28 +0400

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,22 +1,61 @@
-import 'dotenv/config';
-import express from 'express';
-import bodyParser from 'body-parser';
-import userRoutes from './routes/userRoutes';
-import cors from 'cors';
-const app = express();
+import dotenv from 'dotenv'
+import express from 'express'
+import userRoutes from './routes/userRoutes'
+import authRoutes from './routes/authRoutes'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import ConnectMongoDB from './Database/ConnectMongoDB'
+import { errorHandler } from './middlewares/errorMiddleware'
+import { authRateLimiter } from './middlewares/authRateLimiter'
 
-app.use(cors());
-app.use(bodyParser.json());
+dotenv.config()
 
-const PORT = process.env.PORT || 3000;
+export interface UserBasicInfo {
+  _id: string
+  username: string
+  email: string
+  verification: {
+    verified: boolean
+  }
+}
 
-// Mount userRoutes
-app.use('/api/users', userRoutes);
+declare global {
+  namespace Express {
+    interface Response {
+      user?: UserBasicInfo | null
+    }
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
-});
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// Security: Enable additional helmet protections (consider tailoring these based on your needs)
+app.use(helmet.contentSecurityPolicy()) // Content Security Policy for script and resource restrictions
+app.use(helmet.referrerPolicy()) // Prevent referrer leakage
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+)
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
+
+app.use('/api/auth', authRateLimiter, authRoutes)
+// userRoutes
+app.use('/api/users', userRoutes)
+
+app.use(errorHandler)
+
+ConnectMongoDB()
