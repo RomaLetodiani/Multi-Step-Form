80ffe0fbcfdc469f7f293a42e647a5de5569a009
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-19 01:25:45 +0400

    Update authRoutes.ts

--- Backend/src/routes/authRoutes.ts
+++ Backend/src/routes/authRoutes.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import AuthController from '../controllers/AuthController';
+import { authenticateUser } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 const authController = new AuthController();
-// Get user details (Authentication required)
+// Register a user (No Authentication required)
 router.post('/register', authController.registerUser);
 
-// Update a user (Authentication required)
+// Login a user (No Authentication required)
 router.post('/login', authController.loginUser);
 
-// Delete a user (Authentication required)
-router.post('/logout', authController.logoutUser);
+// Logout a user (Authentication required)
+router.post('/logout', authenticateUser, authController.logoutUser);
 
 export default router;
