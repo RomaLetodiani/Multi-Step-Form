e4de92f6a78b98a5ca2ccaab8a148d4ebfc98053
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-19 01:26:34 +0400

    Update AuthServices.ts

--- Backend/src/services/AuthServices.ts
+++ Backend/src/services/AuthServices.ts
@@ -1,68 +1,59 @@
-import { Request, Response } from 'express';
-import { User } from '../models/User';
-import {
-  UserRegistrationError,
-  UserLoginError,
-} from '../middlewares/errorMiddleware';
-import {
-  validateEmail,
-  validateUsername,
-  validatePassword,
-} from '../utils/validation';
-import { clearToken, generateToken } from '../utils/auth';
+import { Request, Response } from 'express'
+import { User } from '../models/User'
+import { UserRegistrationError, UserLoginError } from '../middlewares/errorMiddleware'
+import { validateEmail, validateUsername, validatePassword } from '../utils/validation'
+import { clearToken, generateToken } from '../utils/auth'
 
 class AuthServices {
   async register(req: Request, res: Response) {
-    const { username, email, password } = req.body;
+    const { username, email, password } = req.body
 
     // Validations
-    await validateEmail(email);
-    await validatePassword(password);
-    await validateUsername(username);
+    await validateUsername(username)
 
-    const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ username })
     if (userExists) {
-      throw new UserRegistrationError('User already exists');
+      throw new UserRegistrationError('User already exists')
     }
+
+    await validatePassword(password)
+    await validateEmail(email)
 
     const newUser = await User.create({
       username,
       email,
       password,
-      verified: false,
-    });
+    })
 
     if (newUser) {
-      generateToken(res, newUser._id);
+      generateToken(res, newUser._id)
     } else {
-      throw new UserRegistrationError('Could not Generate Token');
+      throw new UserRegistrationError('Could not Generate Token')
     }
 
-    return newUser;
+    return newUser
   }
   async login(req: Request, res: Response) {
-    const { email, password } = req.body;
+    const { username, password } = req.body
 
     // Validations
-    await validateEmail(email);
-    await validatePassword(password);
+    await validateUsername(username)
 
-    const user = await User.findOne({ email });
-
+    const user = await User.findOne({ username })
     if (!user) {
-      throw new UserLoginError('User not found');
+      throw new UserLoginError('User not found')
     }
 
     if (!(await user.comparePassword(password))) {
-      throw new UserLoginError('Incorrect Password');
+      throw new UserLoginError('Incorrect Password')
     }
 
-    generateToken(res, user._id);
-    return user;
+    generateToken(res, user._id)
+    return user
   }
   async logout(res: Response) {
-    clearToken(res);
+    clearToken(res)
   }
 }
 
-export default AuthServices;
+export default AuthServices
