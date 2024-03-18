78ce8d4a7d1975392b36e6a3dc54387d894f4e61
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-19 01:39:39 +0400

    Update UserController.ts

--- Backend/src/controllers/UserController.ts
+++ Backend/src/controllers/UserController.ts
@@ -1,36 +1,40 @@
 import asyncHandler from 'express-async-handler'
 import { Request, Response } from 'express'
 import UserServices from '../services/UserServices'
-const userServices = new UserServices()
 
 class UserController {
+  private readonly userServices: UserServices
+
+  constructor() {
+    this.userServices = new UserServices()
+  }
   // Method for getting user details
   getUserDetails = asyncHandler(async (__, res: Response) => {
-    const user = await userServices.getDetails(res)
+    const user = await this.userServices.getDetails(res)
     res.status(200).json({ message: 'User details', user })
   })
 
   // Method for updating a user
   updateUser = asyncHandler(async (req: Request, res: Response) => {
-    const updatedUser = await userServices.updateUser(req, res)
+    const updatedUser = await this.userServices.updateUser(req, res)
     res.status(200).json({ message: 'User updated successfully', updatedUser })
   })
 
   // Method for deleting a user
   deleteUser = asyncHandler(async (__, res: Response) => {
-    await userServices.deleteUser(res)
+    await this.userServices.deleteUser(res)
     res.status(200).json({ message: 'User deleted successfully' })
   })
 
   // Method for sending an email
   sendEmail = asyncHandler(async (req: Request, res: Response) => {
-    await userServices.sendEmail(req, res)
+    await this.userServices.sendEmail(req, res)
     res.status(200).json({ message: 'Email sent successfully' })
   })
 
   // Method for Email Verification
   verifyEmail = asyncHandler(async (req: Request, res: Response) => {
-    await userServices.verifyEmail(req, res)
+    await this.userServices.verifyEmail(req, res)
     res.status(200).json({ message: 'Email verified successfully' })
   })
 }
