7ec6a9d2a9ff81f895377f3f5df6992904d51d49
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-07 20:16:43 +0400

    Update userRoutes.ts

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,21 +1,27 @@
 import express from 'express'
 import UserController from '../controllers/UserController'
 import { emailVerificationRateLimiter } from '../middlewares/emailVerificationRateLimiter'
+import { verificationCheckRateLimiter } from '../middlewares/verificationCheckRateLimiter'
 import { authenticateUser } from '../middlewares/authMiddleware'
 
 const router = express.Router()
-const userController = new UserController()
 
 // Get user details (Authentication required)
-router.get('/details', authenticateUser, userController.getUserDetails)
+router.get('/details', authenticateUser, UserController.getUserDetails)
 
 // Update a user (Authentication required)
-router.put('/update', authenticateUser, userController.updateUser)
+router.put('/update', authenticateUser, UserController.updateUser)
 
 // Delete a user (Authentication required)
-router.delete('/', authenticateUser, userController.deleteUser)
+router.delete('/', authenticateUser, UserController.deleteUser)
 
 // Send an verification email (Authentication required)
-router.post('/sendEmail', emailVerificationRateLimiter, authenticateUser, userController.sendEmail)
+router.post('/sendEmail', emailVerificationRateLimiter, authenticateUser, UserController.sendEmail)
+router.post(
+  '/verifyEmail',
+  verificationCheckRateLimiter,
+  authenticateUser,
+  UserController.verifyEmail,
+)
 
 export default router
