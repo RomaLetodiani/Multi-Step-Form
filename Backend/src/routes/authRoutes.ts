0d235117c1c8aee2cd07b8aaaacb04f983d00f65
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-07 20:17:11 +0400

    Update authRoutes.ts

--- Backend/src/routes/authRoutes.ts
+++ Backend/src/routes/authRoutes.ts
@@ -1,16 +1,15 @@
-import express from 'express';
-import AuthController from '../controllers/AuthController';
-import { authenticateUser } from '../middlewares/authMiddleware';
+import express from 'express'
+import AuthController from '../controllers/AuthController'
+import { authenticateUser } from '../middlewares/authMiddleware'
 
-const router = express.Router();
-const authController = new AuthController();
+const router = express.Router()
 // Register a user (No Authentication required)
-router.post('/register', authController.registerUser);
+router.post('/register', AuthController.registerUser)
 
 // Login a user (No Authentication required)
-router.post('/login', authController.loginUser);
+router.post('/login', AuthController.loginUser)
 
 // Logout a user (Authentication required)
-router.post('/logout', authenticateUser, authController.logoutUser);
+router.post('/logout', authenticateUser, AuthController.logoutUser)
 
-export default router;
+export default router
