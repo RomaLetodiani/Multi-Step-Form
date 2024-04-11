8665e4c808b417b10aa6d000940d87768d38d2f1
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-12 02:16:24 +0400

    Vercel Deployment

---
+++ Frontend/src/Pages/Auth/Login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div className="absolute">Login</div>
+}
+
+export default Login
---
+++ Frontend/src/Pages/Auth/Register.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <div className="absolute z-10">teagagaagasg</div>
+}
+
+export default Register
