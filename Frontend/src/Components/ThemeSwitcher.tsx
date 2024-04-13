5ac4a13228dfd96444fb68549c183011c3329169
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-13 10:43:47 +0400

    Update ThemeSwitcher.tsx

--- Frontend/src/Components/ThemeSwitcher.tsx
+++ Frontend/src/Components/ThemeSwitcher.tsx
@@ -1,26 +1,45 @@
 import { useLocation } from 'react-router-dom'
 import { Moon, Sun } from '../Assets/Icons/ThemeSwitcherIcons'
 import useThemeStore from '../Theme/UseThemeStore'
+import { twMerge } from 'tailwind-merge'
+import { useEffect } from 'react'
+import useMediaQuery from '../Hooks/UseMediaQuery'
 
 // Tailwind has to have the classes imported for dynamic use
 // @ts-ignore for the warning: "'tailwindClasses' is declared but its value is never read.ts(6133)"
-const tailwindClasses = [
-  'text-light-container',
-  'text-dark-container',
-  'text-light-text',
-  'text-dark-text',
-]
+const tailwindClasses = ['text-light-btn', 'text-dark-btn']
 const ThemeSwitcher = () => {
   const { pathname } = useLocation()
   const { theme, setTheme } = useThemeStore()
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
+  useEffect(() => {
+    // Set the class on the body element when the component mounts
+    document.body.classList.add(theme)
+    if (pathname !== '/') {
+      document.body.classList.remove('home')
+    } else {
+      document.body.classList.add('home')
+    }
+    return () => {
+      // Clean up class on unmount
+      document.body.classList.remove(theme)
+    }
+  }, [theme, pathname])
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
   return (
     <div
-      className={`absolute w-8 h-8 z-50 ${
-        pathname === '/' ? `right-8 top-8 text-white` : `right-4 top-4 text-${theme}-text`
-      } transition-all ease-in duration-300`}
+      className={twMerge(
+        'text-white absolute w-8 h-8 z-50 right-4 top-4 transition-all ease-in duration-300',
+        isDesktop && `text-${theme}-btn`,
+        pathname === '/' && 'right-8 top-8 text-white',
+      )}
     >
-      {theme === 'light' && <button onClick={() => setTheme('dark')}>{Sun}</button>}
-      {theme === 'dark' && <button onClick={() => setTheme('light')}>{Moon}</button>}
+      <button onClick={toggleTheme}>{theme === 'light' ? Sun : Moon}</button>
     </div>
   )
 }
