e488267a589aa447494ea52183fa146a76b16238
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-13 10:43:20 +0400

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,18 +7,20 @@ export default {
         light: {
           DEFAULT: '#f0f6ff', // Light background
           container: '#f9f9f9', // Light container
+          btn: '#6259FF', // Light button
+          primary: '#483EFF',
 
-          primary: '#483EFF', // Adjust for your primary brand color
           secondary: '#6259FF', // Adjust for your secondary color
           accent: '#F9818E', // Can be used for highlights, buttons, etc.
           white: '#fff',
           text: '#000', // Adjust for good contrast on light background
         },
         dark: {
-          DEFAULT: '#333333', // Dark background
-          container: '#222222', // Dark container
+          DEFAULT: '#0C111D', // Dark background
+          container: '#161B26', // Dark container
+          btn: '#534BEF', // Dark button
+          primary: '#443EB1',
 
-          primary: '#EBF5FF', // Adjust for good contrast on dark background
           secondary: '#D3D9FF', // Adjust for contrast
           accent: '#FFD1D6', // Consider adjusting for contrast
           white: '#fff',
@@ -26,8 +28,12 @@ export default {
         },
       },
       boxShadow: {
-        darks: '0 0 20px -5px rgba(250, 250, 250, 0.25)',
-        lights: '0 0 20px -5px rgba(0, 0, 0, 0.25)',
+        sm: '0 0 20px -5px rgba(0, 0, 0, 0.25)',
+      },
+
+      fontFamily: {
+        seymour: 'Seymour One',
+        ubuntu: 'Ubuntu',
       },
     },
   },
