25f9310adc2e98c454ba0aee0ee10710afc0fd89
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-13 10:43:15 +0400

    Update UseThemeStore.ts

--- Frontend/src/Theme/UseThemeStore.ts
+++ Frontend/src/Theme/UseThemeStore.ts
@@ -1,11 +1,16 @@
 import { create } from 'zustand'
 
-const useThemeStore = create<{ theme: string; setTheme: (state: string) => void }>((set) => ({
-  theme: localStorage.getItem('theme') || 'light', // Initial theme
-  setTheme: (newTheme) => {
-    set(() => ({ theme: newTheme }))
-    localStorage.setItem('theme', newTheme) // Persist to local storage
-  },
-}))
+const useThemeStore = create<{ theme: string; setTheme: (state: string) => void }>((set) => {
+  // Detect user's preferred color scheme (light or dark)
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  return {
+    theme: localStorage.getItem('theme') || (prefersDarkScheme ? 'dark' : 'light'), // Initial theme based on user's preferred color scheme
+    setTheme: (newTheme) => {
+      set(() => ({ theme: newTheme }))
+      localStorage.setItem('theme', newTheme) // Persist to local storage
+    },
+  }
+})
 
 export default useThemeStore
