7ac96304be9943b5fb7bab3c26676e1477c7bb7e
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-13 10:43:52 +0400

    Update Input.tsx

--- Frontend/src/Components/Input.tsx
+++ Frontend/src/Components/Input.tsx
@@ -1,5 +1,6 @@
 import { InputHTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
+import useThemeStore from '../Theme/UseThemeStore'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   // Additional props specific to Input component can be defined here
@@ -10,30 +11,32 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   focus?: boolean
 }
 
+// Tailwind has to have the classes imported for dynamic use
+// @ts-ignore for the warning: "'tailwindClasses' is declared but its value is never read.ts(6133)"
+const tailwindClasses = ['text-light-text', 'text-dark-text']
 const Input = ({ label, hasError, errorMessage, focus, inputClass, ...rest }: InputProps) => {
+  const { theme } = useThemeStore()
   return (
     <div className="relative w-full">
-      {label && (
-        <label
-          className={twMerge(
-            'absolute left-3 pointer-events-none transition-all text-sm translate-x-0 -translate-y-1/2 top-1/2',
-            hasError && 'text-red-500',
-            focus && 'text-[10px] top-1',
-          )}
-          htmlFor={rest.name}
-        >
-          {label}
-        </label>
-      )}
+      <div className="flex justify-between mb-1">
+        {label && (
+          <label className={`text-${theme}-text font-bold`} htmlFor={label}>
+            {label}
+          </label>
+        )}
+        {hasError && <p className="text-red-500 font-bold text-xs mt-2 mr-2">{errorMessage}</p>}
+      </div>
       <input
+        autoComplete="true"
+        id={label}
         className={twMerge(
-          'w-full rounded-lg border border-gray-300 outline-none text-sm',
+          `bg-${theme} text-${theme}-text`,
+          'smooth w-full px-3 py-2 rounded-lg border border-gray-300 outline-none',
           inputClass,
-          hasError && 'border-warm-red text-warm-red',
+          hasError && 'border-red-500 text-red-500',
         )}
         {...rest}
       />
-      {hasError && <p className="absolute -bottom-10">{errorMessage}</p>}
     </div>
   )
 }
