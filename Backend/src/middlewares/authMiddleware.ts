80db0393524abec672cebd221d56f8d182c032ff
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-20 07:32:58 +0400

    Update authMiddleware.ts

--- Backend/src/middlewares/authMiddleware.ts
+++ Backend/src/middlewares/authMiddleware.ts
@@ -3,15 +3,22 @@ import jwt, { JwtPayload } from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
 import { AuthenticationError } from './errorMiddleware'
 import { User } from '../models/User'
+import { getToken } from '../utils/auth'
 
 export const authenticateUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let token = req.cookies.jwt
+      const tokenId = req.cookies.token_id
 
-      if (!token) {
+      if (!tokenId) {
         throw new AuthenticationError('Token is required')
       }
+
+      const token = getToken(tokenId)
+      if (!token) {
+        throw new AuthenticationError('Invalid token')
+      }
+
       const jwtSecret = process.env.JWT_SECRET_KEY || ''
       const decoded = jwt.verify(token, jwtSecret) as JwtPayload
 
@@ -23,9 +30,11 @@ export const authenticateUser = asyncHandler(
         decoded.userId,
         '_id username email verified subscription',
       ).populate('subscription')
+
       if (!user) {
         throw new AuthenticationError('User not found')
       }
+
       res.user = user
       next()
     } catch (err) {
