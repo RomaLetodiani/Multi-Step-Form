de791051fc4af433acaf1da54b8fe821c069e778
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-20 05:44:56 +0400

    Update Profile.tsx

--- Frontend/src/Pages/Profile/Profile.tsx
+++ Frontend/src/Pages/Profile/Profile.tsx
@@ -4,17 +4,58 @@ import useUserStore from '../../Stores/User/UseUserStore'
 
 const Profile = () => {
   const { isAuthenticated, logout } = useAuthStore()
-  console.log('🚀 ~ Profile ~ isAuthenticated:', isAuthenticated)
   const { getDetails, user } = useUserStore()
+  console.log('🚀 ~ Profile ~ user:', user)
+
   useEffect(() => {
-    getDetails()
+    isAuthenticated && getDetails()
   }, [isAuthenticated])
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>{user?.username}</p>
-      <button onClick={logout}>Logout</button>
+    <div className="p-6 rounded-lg">
+      <h1 className="text-2xl font-bold mb-4">Profile</h1>
+      <div className="mb-4">
+        <p className="text-gray-700 font-semibold">Username:</p>
+        <p>{user?.username}</p>
+      </div>
+      <div className="mb-4">
+        <p className="text-gray-700 font-semibold">Email:</p>
+        <p>{user?.email}</p>
+      </div>
+      <div className="mb-4">
+        <p className="text-gray-700 font-semibold">Subscription:</p>
+        {user?.subscription && (
+          <div>
+            <p>Active: {user.subscription.active ? 'Yes' : 'No'}</p>
+            <p>Expires At: {new Date(user.subscription.expiresAt).toLocaleDateString()}</p>
+            <p>Monthly Price: ${user.subscription.monthlyPrice}</p>
+            <p>Name: {user.subscription.name}</p>
+            <p>Type: {user.subscription.type}</p>
+            {user.subscription.addOns && user.subscription.addOns.length > 0 && (
+              <div>
+                <p className="font-semibold">Add-ons:</p>
+                <ul>
+                  {user.subscription.addOns.map((addOn, index) => (
+                    <li key={index}>
+                      <p>
+                        {addOn.name} - ${addOn.price} - {addOn.description}
+                      </p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
