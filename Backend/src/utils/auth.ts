39cfeeec033144398010b8c44d9ddae68338d1fd
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-20 06:33:40 +0400

    Update auth.ts

--- Backend/src/utils/auth.ts
+++ Backend/src/utils/auth.ts
@@ -12,7 +12,7 @@ const generateToken = (res: Response, userId: string) => {
   res.cookie('jwt', token, {
     httpOnly: false,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 60 * 60 * 1000,
   })
 }
