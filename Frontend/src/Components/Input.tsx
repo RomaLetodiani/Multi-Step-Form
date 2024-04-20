29289f8424b3b2cfbcb7530e8077e4b41d2d8c6c
RomaLetodiani <roman.letodiani03@gmail.com>
2024-04-20 15:46:20 +0400

    added password state

--- Frontend/src/Components/Input.tsx
+++ Frontend/src/Components/Input.tsx
@@ -1,6 +1,7 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import useThemeStore from '../Stores/Theme/UseThemeStore'
+import { Eye, eyeSlash } from '../Assets/Icons/eye'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   // Additional props specific to Input component can be defined here
@@ -14,8 +15,9 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 // Tailwind has to have the classes imported for dynamic use
 // @ts-ignore for the warning: "'tailwindClasses' is declared but its value is never read.ts(6133)"
 const tailwindClasses = ['text-light-text', 'text-dark-text']
-const Input = ({ label, hasError, errorMessage, focus, inputClass, ...rest }: InputProps) => {
+const Input = ({ label, hasError, errorMessage, focus, inputClass, type, ...rest }: InputProps) => {
   const { theme } = useThemeStore()
+  const [isPasswordVisible, setIsPasswordVisible] = useState(type !== 'password')
   return (
     <div className="relative w-full">
       <div className="flex justify-between mb-1">
@@ -26,17 +28,35 @@ const Input = ({ label, hasError, errorMessage, focus, inputClass, ...rest }: In
         )}
         {hasError && <p className="text-red-500 font-bold text-xs mt-2 mr-2">{errorMessage}</p>}
       </div>
-      <input
-        autoComplete="true"
-        id={label}
-        className={twMerge(
-          `bg-${theme} text-${theme}-text`,
-          'smooth w-full px-3 py-2 rounded-lg border border-gray-300 outline-none',
-          inputClass,
-          hasError && 'border-red-500 text-red-500',
-        )}
-        {...rest}
-      />
+      {
+        <span className="relative">
+          <input
+            autoComplete="off"
+            id={label}
+            className={twMerge(
+              `bg-${theme} text-${theme}-text`,
+              'smooth w-full px-3 py-2 rounded-lg border border-gray-300 outline-none',
+              inputClass,
+              hasError && 'border-red-500 text-red-500',
+              type === 'password' && 'pr-10',
+            )}
+            type={isPasswordVisible ? 'text' : type}
+            {...rest}
+          />
+          {type === 'password' && (
+            <p
+              onClick={() => setIsPasswordVisible((prev) => !prev)}
+              className={twMerge(
+                'absolute right-0 -top-[1px] -translate-y-1/4 h-10 w-10 flex justify-center cursor-pointer items-center',
+                `text-${theme}-btn`,
+                hasError && 'text-red-500',
+              )}
+            >
+              {isPasswordVisible ? Eye : eyeSlash}
+            </p>
+          )}
+        </span>
+      }
     </div>
   )
 }
