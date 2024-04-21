74af7bdeff8a982ebc72dc5e11190dd2d52c9998
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-21 17:51:05 +0400

    Update UserServices

--- Frontend/src/Services/User.ts
+++ Frontend/src/Services/User.ts
@@ -2,4 +2,6 @@ import api from '../Utils/axiosApi'
 
 export const UserServices = {
   getDetails: () => api.get('users/details'),
+  sendEmailVerification: () => api.post('users/sendEmail'),
+  verifyEmail: (verificationCode: string) => api.post('users/verifyEmail', { verificationCode }),
 }
