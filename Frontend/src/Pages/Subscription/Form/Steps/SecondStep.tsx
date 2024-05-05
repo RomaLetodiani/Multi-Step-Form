e702138828c75d0d467d6589ac10efef2c510246
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-05 10:56:05 +0400

    Update SecondStep.tsx

--- Frontend/src/Pages/Subscription/Form/Steps/SecondStep.tsx
+++ Frontend/src/Pages/Subscription/Form/Steps/SecondStep.tsx
@@ -1,5 +1,28 @@
+import SubPlan from '../../../../Components/SubPlan'
+import ToggleButton from '../../../../Components/ToggleButton'
+import { plans } from '../../../../Shared/Const'
+import useSubStore from '../../../../Stores/Subscription/UseSubStore'
+
 const SecondStep = () => {
-  return <div>SecondStep</div>
+  const { subscriptionName, subscriptionType, setSubscriptionType } = useSubStore()
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col md:flex-row gap-5">
+        {plans.map((plan) => (
+          <SubPlan
+            key={plan.id}
+            active={subscriptionName === plan.name}
+            type={subscriptionType}
+            name={plan.name}
+          />
+        ))}
+      </div>
+      <ToggleButton
+        value={subscriptionType}
+        onClick={() => setSubscriptionType(subscriptionType ? 0 : 1)}
+      />
+    </div>
+  )
 }
 
 export default SecondStep
